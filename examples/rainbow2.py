4c78e3c9d16fad8a92d1883845be57053ac6f10a
Trotter73 <martin_e_coleman@hotmail.com>
2022-11-14 20:27:47 +0000

    Rainbow test

---
+++ examples/rainbow2.py
@@ -0,0 +1,69 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple test for NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 64
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.RGB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+#Replacing loops
+j = 0
+#Keep track of count direction
+direction = 0
+
+while True:
+    Colour = (wheel(j))
+    pixels.fill(Colour)
+    pixels.show()
+
+    print (j)
+
+    #Logic replacing loops
+    if j < 256 and direction == 0:
+        j+=1
+    if j >0 and direction == 1:
+        j-=1
+    if j == 255:
+        direction = 1
+    if j == 0:
+       direction = 0
+
+    time.sleep(0.3)
